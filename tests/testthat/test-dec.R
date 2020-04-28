3a87f83b70ed5a94f2b14590f47beba132f1606d
Are Edvardsen <biorakel@gmail.com>
2020-04-28 12:51:31 +0200

    add tests for decryption

---
+++ tests/testthat/test-dec.R
@@ -0,0 +1,47 @@
+dir <- tempdir()
+dir.create(file.path(dir, "www"))
+content_file <- file.path(dir, "content.csv")
+write.csv(iris, file = content_file)
+key <- openssl::rsa_keygen()
+keyfile <- file.path(dir, "id_rsa")
+openssl::write_pem(key, path = keyfile)
+conf <- get_config()$pubkey$holder$local
+pid <- "unittest"
+pubkey_filename <- paste0(conf$pid$prefix, pid, conf$pid$suffix)
+pubkey_filename <- file.path(dir, "www", pubkey_filename)
+openssl::write_ssh(key$pubkey, path = pubkey_filename)
+
+
+s <- httpuv::startServer(conf$host, port = conf$port,
+  app = list(
+    staticPaths = list("/" = file.path(dir, "www"))
+  )
+)
+
+# make an encrypted file and remove source
+tarfile <- enc(filename = content_file, pubkey_holder = "local", pid = pid)
+file.remove(content_file)
+
+test_that("decryption function returns invisible", {
+  expect_invisible(dec(tarfile, keyfile, dir))
+})
+
+test_that("archive contains expected files", {
+  expect_true(file.exists(file.path(dir, "content.csv")))
+})
+
+test_that("data is identical before (encryption) and after decryption", {
+  write.csv(iris, file = file.path(dir, "iris.csv"))
+  expected <- read.csv(file.path(dir, "iris.csv"))
+  data <- read.csv(file.path(dir, "content.csv"))
+  expect_identical(data, expected)
+})
+
+# clean up
+file.remove(tarfile)
+file.remove(keyfile)
+file.remove(pubkey_filename)
+file.remove(file.path(dir, "iris.csv"))
+file.remove(file.path(dir, "content.csv"))
+file.remove(file.path(dir, "www"))
+s$stop()
