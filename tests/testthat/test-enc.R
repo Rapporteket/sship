79de2a156d619faf3f63c245492f5aab11c77dec
Are Edvardsen <biorakel@gmail.com>
2021-09-03 09:35:47 +0000

    abandon setwd()

--- tests/testthat/test-enc.R
+++ tests/testthat/test-enc.R
@@ -3,12 +3,11 @@ conf <- get_config()$pubkey$holder$local
 
 # set up httpuv serving files from www at tempdir
 dir <- tempdir()
-setwd(dir)
-dir.create("www")
-dir.create("tmp")
+dir.create(file.path(dir, "www"))
+dir.create(file.path(dir, "tmp"))
 s <- httpuv::startServer(conf$host, port = conf$port,
   app = list(
-    staticPaths = list("/" = "www")
+    staticPaths = list("/" = file.path(dir, "www"))
   )
 )
 
@@ -18,8 +17,8 @@ pid <- "unittest"
 pubkey_filename <- paste0(conf$pid$prefix, pid, conf$pid$suffix)
 pubkey_filepath <- file.path(dir, "www", pubkey_filename)
 pubkey_url <- paste0(conf$url, "/", pubkey_filename)
-write.csv(iris, file = "content.csv")
-content_file <- normalizePath("content.csv")
+write.csv(iris, file = file.path(dir, "content.csv"))
+content_file <- normalizePath(file.path(dir, "content.csv"))
 
 
 test_that("a filename for the encrypted file can be provided", {
@@ -46,61 +45,57 @@ key <- openssl::rsa_keygen()
 writeLines(openssl::write_ssh(key$pubkey), pubkey_filepath, sep = "")
 
 test_that("a message is returned", {
-  file.copy("content.csv", "tmp/content.csv")
-  setwd("tmp/")
-  expect_message(enc("content.csv", pubkey_holder = "local", pid = pid))
-  setwd("../.")
-  unlink("tmp/*")
+  file.copy(content_file, file.path(dir, "tmp/content.csv"))
+  expect_message(
+    enc(file.path(dir, "tmp/content.csv"), pubkey_holder = "local", pid = pid))
+  unlink(file.path(dir, "tmp/*"))
 })
 
 test_that("function returns invisibly", {
-  file.copy("content.csv", "tmp/content.csv")
-  setwd("tmp/")
-  expect_invisible(enc("content.csv", pubkey_holder = "local", pid = pid))
-  setwd("../.")
-  unlink("tmp/*")
+  file.copy(content_file, file.path(dir, "tmp/content.csv"))
+  expect_invisible(
+    enc(file.path(dir, "tmp/content.csv"), pubkey_holder = "local", pid = pid))
+  unlink(file.path(dir, "tmp/*"))
 })
 
 test_that("a file name can be obtained from the function", {
-  file.copy("content.csv", "tmp/content.csv")
-  setwd("tmp/")
-  f <- enc("content.csv", pubkey_holder = "local", pid = pid)
+  file.copy(content_file, file.path(dir, "tmp/content.csv"))
+  f <- enc(file.path(dir, "tmp/content.csv"), pubkey_holder = "local",
+           pid = pid)
   expect_true(file.exists(f))
-  setwd("../.")
-  unlink("tmp/*")
+  unlink(file.path(dir, "tmp/*"))
 })
 
 # test warning when multiple public keys available
 pk <- openssl::write_ssh(key$pubkey)
 writeLines(c(pk, pk), pubkey_filepath, sep = "\n")
 test_that("function provide warning when multiple pubkeys", {
-  file.copy("content.csv", "tmp/content.csv")
-  setwd("tmp/")
-  expect_warning(enc("content.csv", pubkey_holder = "local", pid = pid))
-  setwd("../.")
-  unlink("tmp/*")
+  file.copy(content_file, file.path(dir, "tmp/content.csv"))
+  expect_warning(
+    enc(file.path(dir, "tmp/content.csv"), pubkey_holder = "local", pid = pid))
+  unlink(file.path(dir, "tmp/*"))
 })
 
 # test encryption with local pubfile
 conf <- get_config()
-file.copy("content.csv", "tmp/content.csv")
+file.copy(content_file, file.path(dir, "tmp/content.csv"))
 pk <- openssl::write_ssh(key$pubkey)
 local_pubkey_filepath <- file.path(dir, "tmp", conf$pubkey$holder$file$path)
-setwd("tmp/")
 writeLines(pk, local_pubkey_filepath, sep = "")
 # fake config
 conf$pubkey$holder$file$path <- local_pubkey_filepath
-write_config(config = conf)
-conf <- get_config()
+write_config(config = conf, dir = file.path(dir, "tmp"))
+conf <- get_config(dir = file.path(dir, "tmp"))
 test_that("encryption can be made by local pub-file", {
-  expect_invisible(enc("content.csv", pubkey_holder = "file", pid = pid))
-  setwd("../.")
-  unlink("tmp/*")
+  expect_invisible(
+    enc(file.path(dir, "tmp/content.csv"), pubkey_holder = NULL, pid = pid,
+        pubkey = local_pubkey_filepath)
+  )
+  unlink(file.path(dir, "tmp/*"))
 })
 
 # clean up
 file.remove(pubkey_filepath)
-file.remove("content.csv")
-setwd(dir)
-file.remove("www")
+file.remove(file.path(dir, "content.csv"))
+file.remove(file.path(dir, "www"))
 s$stop()
