1a61e8563c0955a82cbbfd5ecc724d403be6a53d
Are Edvardsen <biorakel@gmail.com>
2022-01-28 11:38:19 +0000

    mocking api tests

--- tests/testthat/test-github.R
+++ tests/testthat/test-github.R
@@ -3,7 +3,7 @@
 
 with_mock_dir("gh_api_response", {
 
-  test_that("github_api is class githubApi", {
+  test_that("github_api is class github_api", {
     expect_equal(
       class(github_api("repos/Rapporteket/sship/branches", proxy_url = "")),
       "github_api"
@@ -13,4 +13,25 @@ with_mock_dir("gh_api_response", {
     expect_error(github_api("none/existent/endpoint"), regexp = "404")
   })
 
+  test_that("rates can be reported", {
+    expect_equal(rate_limit()$path, "/rate_limit")
+  })
+
+  test_that("a useful value from the api can be returned", {
+    expect_true("main" %in% gh("repos/Rapporteket/sship/branches")$content$name)
+  })
+
+})
+
+
+# To recreate the stored responses delete the 'gh_api_response_overrated'
+# directory recursively and re-run these tests. They will fail initially, so
+# manually edit gh_api_response_overrated/api.github.com/rate_limit.json and set
+# 'remaining' to 0
+
+with_mock_dir("gh_api_response_overrated", {
+  test_that("gh function ends gracefully when api allowance is exceeded", {
+    expect_message(gh("repos/Rapporteket/sship/branches"))
+    expect_null(gh("repos/Rapporteket/sship/branches"))
+  })
 })
