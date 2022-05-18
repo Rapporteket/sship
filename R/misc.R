e0bcfe43b132a0d8f3cc7824aa16339cdf0ad321
Are Edvardsen <biorakel@gmail.com>
2022-05-18 12:39:53 +0200

    general ssh pubkey type filter

--- R/misc.R
+++ R/misc.R
@@ -67,3 +67,34 @@ keygen <- function(directory = normalizePath("~/.ssh"), type = "rsa",
 
   invisible()
 }
+
+#' Filter ssh public keys by type
+#'
+#' From a vector of ssh public keys, return those that are of a given type.
+#'
+#' @param keys Vector of strings representing ssh public keys.
+#' @param type Character string defining the ssh public key type that will pass
+#' the filter. Relevant values are strings returned by
+#' \code{attributes(openssl::read_pubkey(pubkey))$class[2]}, \emph{e.g.} "rsa"
+#' and "dsa".
+#'
+#'
+#' @return A vector of strings representing (filtered) public keys.
+#' @export
+#'
+#' @examples
+#' ## make ssh public key strings
+#' rsa_pubkey <- openssl::write_ssh(openssl::rsa_keygen()$pubkey)
+#' dsa_pubkey <- openssl::write_ssh(openssl::dsa_keygen()$pubkey)
+#'
+#' ## filter keys by type
+#' pubkey <- pubkey_filter(c(rsa_pubkey, dsa_pubkey), "rsa")
+#' identical(pubkey, rsa_pubkey)
+
+pubkey_filter <- function(keys, type) {
+
+  pass <- function(x, y) attributes(openssl::read_pubkey(x))$class[2] == y
+
+  keys[vapply(keys, pass, logical(length = 1), y = type)]
+}
+
--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -59,3 +59,10 @@ test_that("existing keys will not be overwritten by default", {
 test_that("existing files can be overwritten by force", {
   expect_message(keygen(directory = tempdir(), overwrite_existing = TRUE))
 })
+
+test_that("ssh public keys can be filtered by type", {
+  rsa_pubkey <- openssl::write_ssh(openssl::rsa_keygen()$pubkey)
+  dsa_pubkey <- openssl::write_ssh(openssl::dsa_keygen()$pubkey)
+  pubkey <- pubkey_filter(c(rsa_pubkey, dsa_pubkey), "rsa")
+  expect_identical(pubkey, rsa_pubkey)
+})
